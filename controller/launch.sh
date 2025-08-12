0041dfa38c4f715d2c6856d58bd7f18406d016c8
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-08-12 15:06:49 +0900

    update for honban

--- controller/launch.sh
+++ controller/launch.sh
@@ -4,7 +4,9 @@ go build -o ./secrets/c-plane ./controller/cmd/server
 go build -o ./secrets/controller ./controller/cmd/controller
 go build -o ./secrets/console ./controller/cmd/console
 
-sudo ip netns exec O ./secrets/c-plane &
+#sudo ip netns exec O ./secrets/c-plane &
+./secrets/c-plane &
 CPLANE_PID=$!
 trap "sudo kill $CPLANE_PID" EXIT
-sudo ip netns exec O ./secrets/controller 
+#sudo ip netns exec O ./secrets/controller 
+./secrets/controller 
