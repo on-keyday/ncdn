ebd7576209613ee404b5989f1c45c715de5050c3
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-08-13 09:23:39 +0900

    disable connect

--- l4lb/run-be-prod.sh
+++ l4lb/run-be-prod.sh
@@ -20,4 +20,5 @@ VIP_ADDR="${VIP_ADDR:-192.0.2.10:8888}"
 
 #ip addr add ${VIP_ADDR} dev lo || true
 
-${BIN_DIR}/popcache -controlPlane ${controlPlaneAddr} -originURL ${originAddr} -nodeId ${NODE_ID} -listenAddr ${VIP_ADDR} -certFile ${CA_DIR}/certs/${CA_SERIAL}/server.crt -keyFile ${CA_DIR}/private/${CA_SERIAL}/server.key
+${BIN_DIR}/popcache -controlPlane ${controlPlaneAddr} -originURL ${originAddr} -nodeId ${NODE_ID} -listenAddr ${VIP_ADDR} 
+#-certFile ${CA_DIR}/certs/${CA_SERIAL}/server.crt -keyFile ${CA_DIR}/private/${CA_SERIAL}/server.key
