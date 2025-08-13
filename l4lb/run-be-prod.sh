66f87777a4ba02f475d4d72707a77dc39f925aa5
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-08-13 16:11:15 +0900

    update

--- l4lb/run-be-prod.sh
+++ l4lb/run-be-prod.sh
@@ -16,9 +16,12 @@ go build -o ${BIN_DIR}/popcache ${SRC_DIR}/popcache
 controlPlaneAddr="${CONTROL_PLANE_ADDRESS:-ws://192.168.88.30:8080}"
 originAddr="${ORIGIN_ADDRESS:-http://192.168.88.30:8888/}"
 NODE_ID="${NODE_ID:-C0}"
-VIP_ADDR="${VIP_ADDR:-192.0.2.10:8888}"
+VIP_ADDR="${VIP_ADDR:-192.0.2.10}"
+
+vipSecure="${VIP_ADDR}:443"
+vipInsecure="${VIP_ADDR}:80"
 
 #ip addr add ${VIP_ADDR} dev lo || true
 
-${BIN_DIR}/popcache -controlPlane ${controlPlaneAddr} -originURL ${originAddr} -nodeId ${NODE_ID} -listenAddr ${VIP_ADDR} 
+${BIN_DIR}/popcache -controlPlane ${controlPlaneAddr} -originURL ${originAddr} -nodeId ${NODE_ID} -listenAddr ${vipSecure} -insecureListenAddr ${vipInsecure} 
 #-certFile ${CA_DIR}/certs/${CA_SERIAL}/server.crt -keyFile ${CA_DIR}/private/${CA_SERIAL}/server.key
